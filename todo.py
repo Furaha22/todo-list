c6c3889c878d4781ee804175ce39fb052f9baa02
Furaha22 <93352451+Furaha22@users.noreply.github.com>
2021-11-30 23:29:11 +0000

    made a to do list

---
+++ todo.py
@@ -0,0 +1,4 @@
+myToDo = ['christmas shopping','mini wearth','fresh garland', 'arched ivy plant with twinkle lights', 'christmas cards']
+print(f'{len(myToDo)} thing(s) left to do')
+for task in myToDo: 
+  print(task)
